6006710ceefb4c26a5cd89d5983f1ddb9492fcf4
hirajya <rodneyestrada2425@gmail.com>
2023-12-01 19:15:37 +0800

    Id qrcode done

--- QRCodeGen/qrgen.py
+++ QRCodeGen/qrgen.py
@@ -1,20 +1,43 @@
 import qrcode
+import os
+import re
 
-def generate_qrcode(text):
+def generate_qrcode(links):
 
-    folder_name = "QRpics"
+    folder_path = r"C:\Users\angel\Desktop\tryCOde\QRCodeGen\QRpics"
 
-    qr = qrcode.QRCode(
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for idx, link in enumerate(links):
+        qr = qrcode.QRCode(
         version = 1,
         error_correction = qrcode.constants.ERROR_CORRECT_L,
         box_size = 10,
         border=4
-    )
+        ) 
 
-    qr.add_data(text)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(f"qrcodeimg.png")
+        qr.add_data(link)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        domain_name = extract_keyword(link)
+        img.save(os.path.join(folder_path, f"qrcode_{domain_name}.png"))
+
+def extract_keyword(url):
+    pattern = r'(?:https?:\/\/)?([^\/]+)\/?$'
+    
+    match = re.search(pattern, url)
+    
+    if match:
+        return match.group(1)
+    else:
+        return None  
+
+links = [
+    "https://python.ph/", 
+    "https://reactjs.org.ph/", 
+    "https://devcon.ph/"
+]
 
 
-generate_qrcode("https://python.ph/")
+generate_qrcode(links)
